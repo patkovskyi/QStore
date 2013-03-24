5708b645c146b85de5dd3200b225b666f52bb3bb
Dmytro Patkovskyi <patkovskyi@gmail.com>
2013-03-25 00:13:54 +0200

    Extensions...

---
+++ Code/QStore/Extensions/IListExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace QStore.Extensions
+{
+    using System.Collections.Generic;
+
+    internal static class IListExtensions
+    {
+        internal static int GetUpperIndex<T>(this IList<T> list, IList<int> lowerIndexes, int lowerIndexIndex)
+        {
+            return lowerIndexIndex + 1 < lowerIndexes.Count ? lowerIndexes[lowerIndexIndex + 1] : list.Count;
+        } 
+    }
+}
