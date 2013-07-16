8b2044984548156647cef18711d7024985b0b7a5
Dmytro Patkovskyi <patkovskyi@gmail.com>
2013-07-16 14:57:51 +0300

    Removed QSpell.

--- Code/QStore.Playground/Properties/AssemblyInfo.cs
+++ Code/QStore.Playground/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QSpell")]
+[assembly: AssemblyTitle("QStore.Playground")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("QSpell")]
+[assembly: AssemblyProduct("QStore.Playground")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("69b63971-0c10-4dba-9f57-f4e97584eb9f")]
+[assembly: Guid("aa75eae1-91a1-455c-bfb2-9f2ac2c8615a")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,5 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("QSpell.Tests")]
