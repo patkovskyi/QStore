da89c537c4146d4fd7faa9ea16baef8beb029825
Dmytro Patkovskyi <patkovskyi@gmail.com>
2013-11-27 23:19:49 +0200

    Initial clean-up.

---
+++ Code/QStore/SimpleDictionary.cs
@@ -0,0 +1,56 @@
+﻿namespace QStore
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class SimpleDictionary<T>
+    {
+        public T this[string key]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public static SimpleDictionary<T> Create(IEnumerable<KeyValuePair<string, T>> keyValues)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsKey(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<KeyValuePair<string, T>> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<KeyValuePair<string, T>> GetByPrefix(IEnumerable<char> prefix)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetKeys()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetKeysByPrefix(IEnumerable<char> keyPrefix)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T TryGetValue(string key, out T value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Code/QStore/SimpleSet.cs
@@ -0,0 +1,29 @@
+﻿namespace QStore
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Xml.Serialization;
+
+    public class SimpleSet
+    {        
+        public static SimpleSet Create(IEnumerable<string> keys)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetByPrefix(IEnumerable<char> keyPrefix)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
